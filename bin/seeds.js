19263fb5dbccea14dd9b906b417309bc7f511e25
Elba Chimilio <elbachimilio@gmail.com>
2019-09-23 14:17:58 -0400

    Deployed to Heroku

---
+++ bin/seeds.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(`${process.env.MONGODB_URI}`, {useNewUrlParser: true})
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
