b73223d1a1b91c305d59af6551c5c26f22516578
Elba Chimilio <elbachimilio@gmail.com>
2019-09-23 15:50:40 -0400

    Added navbar

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect(`${process.env.MONGODB_URI}`, {useNewUrlParser: true})
+  .connect(`mongodb://localhost/airBnB`, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
