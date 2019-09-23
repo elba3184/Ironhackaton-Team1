e7864aa008b92882adb7f31c48d4042f174055f7
Cole Mooney <colemooney@Coles-MacBook-Pro.local>
2019-09-23 15:47:02 -0400

    login/signup

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const Test = require('../models/Test');
 
 mongoose
-  .connect(`${process.env.MONGODB_URI}`, {useNewUrlParser: true})
+  .connect('mongodb://localhost/airbnb', {useNewUrlParser: true})
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+const test = [
+ {
+  first_name: "Cole",
+  last_name: "Mooney"
+ }
+]
+
+Test.create(test);
